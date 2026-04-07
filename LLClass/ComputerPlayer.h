98f3f5ce032f94f89662eb612a3f22bbe0de73fe
Steffen T <steffmturner94@gmail.com>
2026-04-06 19:24:20 -0500

    Added declarations in ComputerPlayer.h that will presumabely be used for the ComputerPlayer class

--- LLClass/ComputerPlayer.h
+++ LLClass/ComputerPlayer.h
@@ -13,9 +13,16 @@
 	//Lose lives
 class ComputerPlayer {
 public:
+	void DrawCard(); //Function to draw a card and add it to the computer's hand
+	void PlayCard(); //Function to play a card from the computer's hand
+	void Bluff(); //Function to bluff by playing a card that does not match the current rank
+	void CallLiar(); //Function to call liar on the previous player
+	string ShowHand(); //Function to show the computer's hand, will also declare if the computer is bluffing or not
 
 private:
 	//Adjustments may need to be made based on Deck/Playing coding
 	string name;
-
+	int lives = 3; //Set number of lives to 3 for now, may change later
+	string hand = []; //Assuming we will be using an array for hands
+	int brain = 0; //Placeholder for the random number generator that will be used to determine if the computer lies or calls
 };
